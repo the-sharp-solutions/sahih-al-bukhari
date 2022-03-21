b4a964c9b4c8d206ff399f12b4289d4754f20964
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-03-22 03:37:40 +0600

    start to test out f.

--- src/components/Header.js
+++ src/components/Header.js
@@ -90,7 +90,7 @@ const Header = ({ location, isDarkThemeActive, toggleActiveTheme }) => (
         },
       } = data;
 
-      const finalLogoLink = logo.link !== '' ? logo.link : 'https://hasura.io/';
+      const finalLogoLink = logo.link !== '' ? logo.link : 'https://sahihalbukhari.gatsbyjs.io';
 
       return (
         <div className={'navBarWrapper'}>
